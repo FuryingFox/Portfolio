060fd345f24d8735248c05f12b7394d08c33d7be
FuryingFox <furyingfox@gmail.com>
2020-08-22 21:26:08 +0200

    Saves when someone sets theme of site

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,39 @@
+window.onload = function(){ 
+    var checkBox = document.getElementById("changetheme");
+    var checked = JSON.parse(localStorage.getItem('changetheme'));
+    document.getElementById("changetheme").checked = checked;
+
+    if (checkBox.checked == true){
+        body.classList.add('bodydark')
+        body.classList.remove('bodylight')
+        document.getElementById("logo").src = "src/img/icon_dark.png";
+    } else {
+        body.classList.add('bodylight')
+        body.classList.remove('bodydark')
+        document.getElementById("logo").src = "src/img/icon_light.png";
+    }
+}
+
 function changeTheme() {
     // Get the checkbox
 var checkBox = document.getElementById("changetheme");
   
 // If the checkbox is checked, display the output text
 if (checkBox.checked == true){
+
     body.classList.add('bodydark')
     body.classList.remove('bodylight')
     document.getElementById("logo").src = "src/img/icon_dark.png";
+
+    localStorage.setItem("changetheme", checkBox.checked)
 } else {
     body.classList.add('bodylight')
     body.classList.remove('bodydark')
     document.getElementById("logo").src = "src/img/icon_light.png";
+
+    localStorage.setItem("changetheme", checkBox.checked)
 }
-}
-       
+}     
+
+
+
