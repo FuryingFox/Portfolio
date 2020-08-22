2836f23ad9a63be8e066083d2a94f94a001b94d2
FuryingFox <furyingfox@gmail.com>
2020-08-22 19:21:04 +0200

    Added toggle switch :+1:

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,16 @@
-window.onload=function(){
-    document.getElementById('changetheme').addEventListener('click', function () {
-        body.classList.add('bodydark')
-        body.classList.remove('bodylight')
-        document.getElementById("logo").src = "src/img/icon_dark.png";
-    });
+function changeTheme() {
+    // Get the checkbox
+var checkBox = document.getElementById("changetheme");
+  
+// If the checkbox is checked, display the output text
+if (checkBox.checked == true){
+    body.classList.add('bodydark')
+    body.classList.remove('bodylight')
+    document.getElementById("logo").src = "src/img/icon_dark.png";
+} else {
+    body.classList.add('bodylight')
+    body.classList.remove('bodydark')
+    document.getElementById("logo").src = "src/img/icon_light.png";
 }
+}
+       
